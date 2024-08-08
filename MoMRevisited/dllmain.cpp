c0bf21d58532264afd98e191d3a6ec485730af1a
RFC1920 <desolationoutpostpve@gmail.com>
2024-08-08 08:14:54 -0500

    Add project files.

---
+++ MoMRevisited/dllmain.cpp
@@ -0,0 +1,40 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+#include "MinHook.h"
+#include <thread>
+
+void* origApproveLogin = nullptr;
+
+static bool ApproveLoginHook(void* param1, void* param2, void* param_3){
+	return true;
+}
+
+static void InitHooking() {
+	uintptr_t baseaddress = (uintptr_t)GetModuleHandleA("MemoriesOfMarsServer.exe");
+
+	MH_Initialize();
+
+	LPVOID approveLogin = (LPVOID)(baseaddress + 0x880050);
+
+	MH_CreateHook(approveLogin, ApproveLoginHook, &origApproveLogin);
+
+	MH_EnableHook(approveLogin);
+}
+
+static void ServerPatchMainThread() {
+	InitHooking();
+}
+
+static BOOL APIENTRY DllMain( HMODULE hModule,
+		DWORD  ul_reason_for_call,
+		LPVOID lpReservedB
+		){
+
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+		std::thread t(ServerPatchMainThread);
+
+		t.detach();
+	}
+
+	return TRUE;
+}
