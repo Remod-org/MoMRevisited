38fb3652ae4c0507eb70072789d1d3b2a6f35f43
RFC1920 <42175991+RFC1920@users.noreply.github.com>
2024-08-08 08:53:14 -0500

    Update game.bat
    Update for standard install dir

--- game.bat
+++ game.bat
@@ -1,2 +1,2 @@
 set OPENSSL_ia32cap=:~0x20000000
-"D:\SteamLibrary\steamapps\common\Memories of Mars\MarsClient\Game\Binaries\Win64\MemoriesOfMars.exe" -noeac
+"C:\Program Files (x86)\Steam\steamapps\common\Memories of Mars\MarsClient\Game\Binaries\Win64\MemoriesOfMars.exe" -noeac
