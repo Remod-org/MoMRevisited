5efc7215d3497293dadbd5f18c3fbdbcf0375bff
RFC1920 <42175991+RFC1920@users.noreply.github.com>
2024-08-17 04:33:19 -0500

    Update server.bat

--- server.bat
+++ server.bat
@@ -1,4 +1,4 @@
 set OPENSSL_ia32cap=:~0x20000000
-START /B "" "D:\SteamLibrary\steamapps\common\Memories of Mars - Dedicated Server\Game\Binaries\Win64\MemoriesOfMarsServer.exe"
+START /B "" "C:\Program Files (x86)\Steam\steamapps\common\Memories of Mars - Dedicated Server\Game\Binaries\Win64\MemoriesOfMarsServer.exe"
 sleep 15s
 Injector.exe --process-name MemoriesOfMarsServer.exe --inject MomRevisited.dll
