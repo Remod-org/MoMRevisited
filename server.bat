39ae6334c127af916760517c51a05913553529fc
RFC1920 <42175991+RFC1920@users.noreply.github.com>
2024-12-15 07:22:55 -0600

    Update server.bat to use valid pause before injection

--- server.bat
+++ server.bat
@@ -1,4 +1,4 @@
 set OPENSSL_ia32cap=:~0x20000000
 START /B "" "C:\Program Files (x86)\Steam\steamapps\common\Memories of Mars - Dedicated Server\Game\Binaries\Win64\MemoriesOfMarsServer.exe"
-sleep 15s
+timeout /t 15 /nobreak
 Injector.exe --process-name MemoriesOfMarsServer.exe --inject MomRevisited.dll
